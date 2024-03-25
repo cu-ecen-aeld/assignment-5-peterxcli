811d4fd87856913f1f93c0b34acf660435aa2ab8
peterxcli <peterxcli@gmail.com>
2024-03-25 07:33:19 +0000

    fix: Update AESD_ASSIGNMENTS_VERSION in aesd-assignments.mk

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '4ed814ce634f43fcbf9a74e04fcaf2b7fc767175'
+AESD_ASSIGNMENTS_VERSION = '00eaea273fdfc1587dab457420a11a565fe7dde7'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
