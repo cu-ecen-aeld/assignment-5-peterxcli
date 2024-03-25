9f31ca672e02b0550895e6927cc57508a558b956
peterxcli <peterxcli@gmail.com>
2024-03-25 08:03:26 +0000

    perf: Update build.sh script to use multiple cores for faster compilation

--- build.sh
+++ build.sh
@@ -9,6 +9,8 @@ git submodule init
 git submodule sync
 git submodule update
 
+NUM_CORES=$(nproc)
+
 set -e 
 cd `dirname $0`
 
@@ -19,15 +21,15 @@ then
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
+		make -C buildroot -j$((NUM_CORES + 1)) defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT} BR2_CCACHE=y
 	else
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
-		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
+		make -C buildroot -j$((NUM_CORES + 1)) defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG} BR2_CCACHE=y
 	fi
 else
 	echo "USING EXISTING BUILDROOT CONFIG"
 	echo "To force update, delete .config or make changes using make menuconfig and build again."
-	make -C buildroot BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT}
+	make -C buildroot -j$((NUM_CORES + 1)) BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_CCACHE=y
 
 fi
